9ee50243d0c38bb26f6c99cc6d42fa226725aca0
liupenghui <liupenghui@58.com>
2021-03-12 23:41:28 +0800

    添加 minglin

--- ShellLearn/shell1.sh
+++ ShellLearn/shell1.sh
@@ -198,3 +198,302 @@ then
 else
    echo "$a -le $b: a 大于 b"
 fi
+
+
+#--------------------------------------------
+#布尔运算符
+#运算符    说明    举例
+#!    非运算，表达式为 true 则返回 false，否则返回 true。    [ ! false ] 返回 true。
+#-o    或运算，有一个表达式为 true 则返回 true。    [ $a -lt 20 -o $b -gt 100 ] 返回 true。
+#-a    与运算，两个表达式都为 true 才返回 true。    [ $a -lt 20 -a $b -gt 100 ] 返回 false。
+#--------------------------------------------
+echo "-------------布尔运算符---------------"
+
+if [ $a != $b ]
+then
+   echo "$a != $b : a 不等于 b"
+else
+   echo "$a == $b: a 等于 b"
+fi
+if [ $a -lt 100 -a $b -gt 15 ]
+then
+   echo "$a 小于 100 且 $b 大于 15 : 返回 true"
+else
+   echo "$a 小于 100 且 $b 大于 15 : 返回 false"
+fi
+if [ $a -lt 100 -o $b -gt 100 ]
+then
+   echo "$a 小于 100 或 $b 大于 100 : 返回 true"
+else
+   echo "$a 小于 100 或 $b 大于 100 : 返回 false"
+fi
+if [ $a -lt 5 -o $b -gt 100 ]
+then
+   echo "$a 小于 5 或 $b 大于 100 : 返回 true"
+else
+   echo "$a 小于 5 或 $b 大于 100 : 返回 false"
+fi
+
+
+
+#--------------------------------------------
+#逻辑运算符
+#运算符    说明    举例
+#&&    逻辑的 AND    [[ $a -lt 100 && $b -gt 100 ]] 返回 false
+#||    逻辑的 OR    [[ $a -lt 100 || $b -gt 100 ]] 返回 true
+#--------------------------------------------
+echo "-------------#逻辑运算符---------------"
+
+if [[ $a -lt 100 && $b -gt 100 ]] #注意⚠️ 必须有内部[] 也不能这样搞 [[ $a -lt 100 ] && [ $b -gt 100 ]]
+then
+   echo "$a 小于100 并且 $b 大于100 ： 返回true"
+else
+   echo "$a 小于100 并且 $b 大于100 ： 返回false"
+fi
+
+if [[ $a -lt 100 || $b -gt 100 ]]
+then
+   echo "$a 小于100 或者 $b 大于100 ： 返回true"
+else
+   echo "$a 小于100 或者 $b 大于100 ： 返回false"
+fi
+
+
+
+
+#-----------------------------------------
+#字符串运算符
+#=    检测两个字符串是否相等，相等返回 true。    [ $a = $b ] 返回 false。
+#!=    检测两个字符串是否不相等，不相等返回 true。    [ $a != $b ] 返回 true。
+#-z    检测字符串长度是否为0，为0返回 true。    [ -z $a ] 返回 false。
+#-n    检测字符串长度是否不为 0，不为 0 返回 true。    [ -n "$a" ] 返回 true。
+#$    检测字符串是否为空，不为空返回 true。    [ $a ] 返回 true。
+#-----------------------------------------
+echo "-------------字符串运算符---------------"
+a="abc"
+b="efg"
+
+if [ $a = $b ]
+then
+   echo "$a = $b : a 等于 b"
+else
+   echo "$a = $b: a 不等于 b"
+fi
+if [ $a != $b ]
+then
+   echo "$a != $b : a 不等于 b"
+else
+   echo "$a != $b: a 等于 b"
+fi
+if [ -z $a ]
+then
+   echo "-z $a : 字符串长度为 0"
+else
+   echo "-z $a : 字符串长度不为 0"
+fi
+if [ -n "$a" ]
+then
+   echo "-n $a : 字符串长度不为 0"
+else
+   echo "-n $a : 字符串长度为 0"
+fi
+if [ $a ]
+then
+   echo "$a : 字符串不为空"
+else
+   echo "$a : 字符串为空"
+fi
+
+
+
+
+
+#-----------------------------------------
+#文件测试运算符
+#操作符    说明    举例
+#-b file    检测文件是否是块设备文件，如果是，则返回 true。    [ -b $file ] 返回 false。
+#-c file    检测文件是否是字符设备文件，如果是，则返回 true。    [ -c $file ] 返回 false。
+#-d file    检测文件是否是目录，如果是，则返回 true。    [ -d $file ] 返回 false。
+#-f file    检测文件是否是普通文件（既不是目录，也不是设备文件），如果是，则返回 true。    [ -f $file ] 返回 true。
+#-g file    检测文件是否设置了 SGID 位，如果是，则返回 true。    [ -g $file ] 返回 false。
+#-k file    检测文件是否设置了粘着位(Sticky Bit)，如果是，则返回 true。    [ -k $file ] 返回 false。
+#-p file    检测文件是否是有名管道，如果是，则返回 true。    [ -p $file ] 返回 false。
+#-u file    检测文件是否设置了 SUID 位，如果是，则返回 true。    [ -u $file ] 返回 false。
+#-r file    检测文件是否可读，如果是，则返回 true。    [ -r $file ] 返回 true。
+#-w file    检测文件是否可写，如果是，则返回 true。    [ -w $file ] 返回 true。
+#-x file    检测文件是否可执行，如果是，则返回 true。    [ -x $file ] 返回 true。
+#-s file    检测文件是否为空（文件大小是否大于0），不为空返回 true。    [ -s $file ] 返回 true。
+#-e file    检测文件（包括目录）是否存在，如果是，则返回 true。    [ -e $file ] 返回 true。
+#其他检查符：
+#
+#-S: 判断某文件是否 socket。
+#-L: 检测文件是否存在并且是一个符号链接。
+#-----------------------------------------
+echo "--------------------------------文件可读--------------------------------"
+
+project_path=$(cd `dirname $0`; pwd)
+
+file="$project_path/shell1.sh"
+
+echo "$file"
+
+
+if [ -r $file ]
+then
+   echo "文件可读"
+else
+   echo "文件不可读"
+fi
+if [ -w $file ]
+then
+   echo "文件可写"
+else
+   echo "文件不可写"
+fi
+if [ -x $file ]
+then
+   echo "文件可执行"
+else
+   echo "文件不可执行"
+fi
+if [ -f $file ]
+then
+   echo "文件为普通文件"
+else
+   echo "文件为特殊文件"
+fi
+if [ -d $file ]
+then
+   echo "文件是个目录"
+else
+   echo "文件不是个目录"
+fi
+if [ -s $file ]
+then
+   echo "文件不为空"
+else
+   echo "文件为空"
+fi
+if [ -e $file ]
+then
+   echo "文件存在"
+else
+   echo "文件不存在"
+fi
+
+#---------------------echo命令-----------------
+#read name
+#echo "$name Is a test"
+#
+#echo -e "OK! \n" #-e 开启转义
+#echo "It is a test"
+
+echo '$your_name\"#$%^'
+
+echo `date`
+
+
+
+#---------------------Shell printf 命令-----------------
+echo "Hello, Shell"
+printf "Hello, Shell\n"
+
+
+printf "%-10s %-8s %-4s\n" 姓名 性别  体重"(KG)"
+printf "%-10s %-8s %-4.2f\n" 朱元璋 男  67.23343
+printf "%-10s %-8s %-4.3f\n" 猪八戒 无  234.4556
+
+
+
+#----------------------------------------------------
+#test 命令-
+#----------------------------------------------------
+num1=10
+num2=100
+if test $[num1] -eq $[num2]
+then
+    echo '两个数相等！'
+else
+    echo '两个数不相等！'
+fi
+
+
+result=$[num1+num2]
+echo "结果：$result"
+
+
+int=1
+while(( $int<=5 )) #()内部括号不可省略
+do
+    echo $int
+    let "int++"  #重新赋值
+done
+
+
+#echo '按下 <CTRL-D> 退出'
+#echo -n '输入你最喜欢的网站名: '
+#while read FILM
+#do
+#    echo "是的！$FILM 是一个好网站"
+#done
+
+
+#while true
+#do
+#  echo "呵"
+#done
+
+
+
+
+#while :
+#do
+#    echo -n "输入 1 到 5 之间的数字:"
+#    read aNum
+#    case $aNum in
+#        1|2|3|4|5) echo "你输入的数字为 $aNum!"
+#        ;;
+#        *) echo "你输入的数字不是 1 到 5 之间的! 游戏结束"
+#            break
+#        ;;
+#    esac
+#done
+
+#修改如下   "游戏结束" 永远不会执行
+#while :
+#do
+#    echo -n "输入 1 到 5 之间的数字: "
+#    read aNum
+#    case $aNum in
+#        1|2|3|4|5) echo "你输入的数字为 $aNum!"
+#        ;;
+#        *) echo "你输入的数字不是 1 到 5 之间的!"
+#            continue
+#            echo "游戏结束"
+#        ;;
+#    esac
+#done
+
+
+#----------------------------------------------------
+# 函数
+#----------------------------------------------------
+demoFun(){
+    echo "这是我的第一个 shell 函数!"
+}
+echo "-----函数开始执行-----"
+demoFun
+echo "-----函数执行完毕-----"
+
+
+funWithReturn(){
+    echo "这个函数会对输入的两个数字进行相加运算..."
+    echo "输入第一个数字: "
+    read aNum
+    echo "输入第二个数字: "
+    read anotherNum
+    echo "两个数字分别为 $aNum 和 $anotherNum !"
+    return $(($aNum+$anotherNum))
+}
+funWithReturn
+echo "输入的两个数字之和为 $? !" #函数返回值在调用该函数后通过 $? 来获得。
