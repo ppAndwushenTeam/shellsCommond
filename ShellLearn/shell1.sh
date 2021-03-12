cb40d71fc3a467fc5939015ad976346634f7dc7a
liupenghui <liupenghui@58.com>
2021-03-12 12:52:40 +0800

    添加shell 菜鸟教程学习记录shelle1

--- ShellLearn/shell1.sh
+++ ShellLearn/shell1.sh
@@ -107,3 +107,91 @@ echo "-- \$@ 演示 ---"
 for i in "$@"; do
     echo $i
 done
+
+
+#--------------------------------------------
+#数组
+#--------------------------------------------
+echo "数组的元素为: ${array_name[*]}"
+echo "数组的元素为: ${array_name[@]}"
+
+echo "数组元素个数为: ${#array_name[*]}"
+echo "数组元素个数为: ${#array_name[@]}"
+
+
+#--------------------------------------------
+#数据运算
+#--------------------------------------------
+val=`expr 3 + 2`
+echo $val
+
+a=10
+b=20
+
+val=`expr $a + $b`
+echo "a + b : $val"
+
+val=`expr $a - $b`
+echo "a - b : $val"
+
+val=`expr $a \* $b`
+echo "a * b : $val"
+
+val=`expr $b / $a`
+echo "b / a : $val"
+
+val=`expr $b % $a`
+echo "b % a : $val"
+
+if [ $a == $b ]
+then
+   echo "a 等于 b"
+fi
+if [ $a != $b ]
+then
+   echo "a 不等于 b"
+fi
+
+
+#--------------------------------------------
+#关系运算符  关系运算符只支持数字，不支持字符串，除非字符串的值是数字。
+#--------------------------------------------
+
+
+
+if [ $a -eq $b ]
+then
+   echo "$a -eq $b : a 等于 b"
+else
+   echo "$a -eq $b: a 不等于 b"
+fi
+if [ $a -ne $b ]
+then
+   echo "$a -ne $b: a 不等于 b"
+else
+   echo "$a -ne $b : a 等于 b"
+fi
+if [ $a -gt $b ]
+then
+   echo "$a -gt $b: a 大于 b"
+else
+   echo "$a -gt $b: a 不大于 b"
+fi
+if [ $a -lt $b ]
+then
+   echo "$a -lt $b: a 小于 b"
+else
+   echo "$a -lt $b: a 不小于 b"
+fi
+if [ $a -ge $b ]
+then
+   echo "$a -ge $b: a 大于或等于 b"
+else
+   echo "$a -ge $b: a 小于 b"
+fi
+if [ $a -le $b ]
+then
+   echo "$a -le $b: a 小于或等于 b"
+else
+   echo "$a -le $b: a 大于 b"
+fi
